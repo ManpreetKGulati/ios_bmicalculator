8f8b9233ae39dec2b6cdd425674660d581aeccc5
“MANPREET <gulatimk.ca@gmail.com>
2024-09-27 12:59:40 -0400

    Initial Commit

---
+++ Assignment1_MLTests/Assignment1_MLTests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1_MLTests.swift
+//  Assignment1_MLTests
+//
+//  Created by Manpreet Gulati on 27/09/24.
+//
+
+import Testing
+@testable import Assignment1_ML
+
+struct Assignment1_MLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
